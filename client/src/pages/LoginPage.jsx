f0c0bc04b2cce8ccf21416097ab21251eacd81cd
Kristian Guzman <nefer6457@gmail.com>
2023-12-22 12:59:26 -0500

    final

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,56 +1,53 @@
 import React from "react";
-import { useState } from 'react'
+import { useState } from 'react';
 import { Link, useNavigate, useOutletContext } from "react-router-dom";
 
-function LoginPage(){
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const { attemptLogin, logout } = useOutletContext();
 
-    const {attemptLogin, logout} = useOutletContext()
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const handleChangeUsername = (e) => setUsername(e.target.value);
+  const handleChangePassword = (e) => setPassword(e.target.value);
 
-    const handleChangeUsername = e => setUsername(e.target.value)
-    const handleChangePassword = e => setPassword(e.target.value)
+  function handleSubmit(e) {
+    e.preventDefault();
+    attemptLogin({ username, password });
+    navigate('/');
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        attemptLogin({username, password})
-        navigate('/')
-    }
-
-    return (
-        <>
-            <form 
-            className="user-form"
-            onSubmit={handleSubmit}
-            >
-                <input
-                type="text"
-                onChange={handleChangeUsername}
-                value={username}
-                // name="username"
-                placeholder="Username..."
-                />
-                <input
-                type="text"
-                onChange={handleChangePassword}
-                value={password}
-                // name="password"
-                placeholder="Password..."
-                />
-                <button
-                type="submit"
-                value='Login'
-                >
-                Login
-                </button>
-            </form>
-            <p>If you don't have a Login, please <Link to="/signup" className="instring-button">Signup</Link></p>
-            
-        </>
-    )
+  return (
+    <>
+      <form className="user-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={handleChangeUsername}
+          value={username}
+          placeholder="Username..."
+        />
+        <input
+          type="text"
+          onChange={handleChangePassword}
+          value={password}
+          placeholder="Password..."
+        />
+        <button type="submit" value="Login">
+          Login
+        </button>
+      </form>
+      <div className="instring-button-container">
+        <p>
+          If you don't have a Login, please{" "}
+          <Link to="/signup" className="instring-button">
+            Signup
+          </Link>
+        </p>
+      </div>
+    </>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
